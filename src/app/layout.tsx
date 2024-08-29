bb0755f1ec3911ec3afa2ed165e1b0273451bf1f
Jay <jay.sanjay.karia@gmail.com>
2024-08-29 08:06:50 +0530

    chore: initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Tasks',
+  description: '📝 A simple task manager',
+  icons: [
+    {
+      href: '/logo.png',
+      url: '/logo.png',
+    },
+  ],
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
